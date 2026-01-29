9cc203bd2316714c1da40df279c853e5e1235d1e
akshi2006 <akshithaa2006@gmail.com>
2026-01-29 15:04:06 +0530

    Add files via upload

---
+++ PPT_Codes/Control Statements/Iteration Statments/Calculator2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Calculator2{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double b = sc.nextDouble();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        char op = sc.next().charAt(0);
+
+        if (op == '+') {
+            System.out.println("Result = " + (a + b));
+        } 
+        else if (op == '-') {
+            System.out.println("Result = " + (a - b));
+        } 
+        else if (op == '*') {
+            System.out.println("Result = " + (a * b));
+        } 
+        else if (op == '/') {
+            if (b != 0)
+                System.out.println("Result = " + (a / b));
+            else
+                System.out.println("Division by zero not allowed");
+        } 
+        else {
+            System.out.println("Invalid operator");
+        }
+    }
+}
